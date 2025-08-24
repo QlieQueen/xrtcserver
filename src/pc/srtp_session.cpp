c4425396543176ecdd9adc217885615923fc1cbb
OlieQueen <qq2841015@163.com>
2025-08-24 23:13:59 +0800

    Add: XRTC1.0-创建Srtp会话并设置参数

---
+++ src/pc/srtp_session.cpp
@@ -0,0 +1,13 @@
+#include "pc/srtp_session.h"
+
+namespace xrtc {
+
+SrtpSession::SrtpSession() {
+
+}
+
+SrtpSession::~SrtpSession() {
+    
+}
+
+} // namespace xrtc
---
+++ src/pc/srtp_session.h
@@ -0,0 +1,14 @@
+#ifndef __SRTP_SESSION_H_
+#define __SRTP_SESSION_H_
+
+namespace xrtc {
+
+class SrtpSession {
+public:
+    SrtpSession();
+    ~SrtpSession();
+};
+
+}
+
+#endif
