ad8bbdeffce6617a12fb42262210ef948eeb472c
OlieQueen <qq2841015@163.com>
2025-08-25 23:05:12 +0800

    Add: XRTC1.0-初始化libsrtp库

--- src/pc/srtp_session.cpp
+++ src/pc/srtp_session.cpp
@@ -1,5 +1,7 @@
+#include <rtc_base/logging.h>
+#include <rtc_base/synchronization/mutex.h>
+#include <absl/base/attributes.h>
 #include "pc/srtp_session.h"
-#include "rtc_base/logging.h"
 #include "srtp2/srtp.h"
 
 namespace xrtc {
@@ -18,7 +20,54 @@ bool SrtpSession::set_send(int cs, const uint8_t* key, size_t key_len,
     return _set_key(ssrc_any_outbound, cs, key, key_len, extension_ids);
 }
 
+ABSL_CONST_INIT int g_libsrtp_usage_count = 0;
+ABSL_CONST_INIT webrtc::GlobalMutex g_libsrtp_lock(absl::kConstInit);
+
+void SrtpSession::_event_handle_thunk(srtp_event_data_t* ev) {
+    SrtpSession* session = (SrtpSession*)(srtp_get_user_data(ev->session));
+    if (session) {
+        session->_handle_event(ev);
+    }
+}
+
+void SrtpSession::_handle_event(srtp_event_data_t* ev) {
+    switch (ev->event) {
+        case event_ssrc_collision:
+            RTC_LOG(LS_INFO) << "SRTP event: ssrc collision";
+            break;
+        case event_key_soft_limit:
+            RTC_LOG(LS_INFO) << "SRTP event: reached key soft limit";
+            break;
+        case event_key_hard_limit:
+            RTC_LOG(LS_INFO) << "SRTP event: reached key hard limit";
+            break;
+        case event_packet_index_limit:
+            RTC_LOG(LS_INFO) << "SRTP event: packet index limit";
+            break;
+        default:
+            RTC_LOG(LS_WARNING) << "SRTP unknown event: " << ev->event;
+            break;
+    }
+}
+
 bool SrtpSession::_increment_libsrtp_usage_count_and_maybe_init() {
+    webrtc::GlobalMutexLock ls(&g_libsrtp_lock);
+
+    if (0 == g_libsrtp_usage_count) {
+        int err = srtp_init();
+        if (err != srtp_err_status_ok) {
+            RTC_LOG(LS_WARNING) << "Failed to init srtp, err: " << err;
+            return false;
+        }
+
+        err = srtp_install_event_handler(&SrtpSession::_event_handle_thunk);
+        if (err != srtp_err_status_ok) {
+            RTC_LOG(LS_WARNING) << "Failed to install srtp event, err: " << err;
+            return false;
+        }
+    }
+
+    g_libsrtp_usage_count++;
     return true;
 }
 
